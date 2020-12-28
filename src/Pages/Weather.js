7056ddfe480943b95d4275ef7eab8aec6e3e8480
chasin87 <yasinyuksek87@gmail.com>
2020-12-28 15:41:15 +0100

    add geo location to app to get current location weather

--- src/Pages/Weather.js
+++ src/Pages/Weather.js
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "@material-ui/core";
 import Searchresult from "./Searchresult";
 import axios from "axios";
 import "./Weather.css";
+import Geocode from "react-geocode";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
-  const [city, setCity] = useState(props.defaultCity);
+  const [city, setCity] = useState(false);
+  const [searcher, setSearcher] = useState();
+  // const [current, setCurrent] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
+      feels_like: response.data.main.feels_like,
       city: response.data.name,
       country: response.data.sys.country,
       icon: response.data.weather[0].icon,
@@ -20,27 +23,82 @@ export default function Weather(props) {
     });
   }
 
-  function search() {
-    const apiKey = "e1f6338909b15ed65e927cc411d39098";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
+  Geocode.setApiKey("AIzaSyC52pIR56WEwdey8vyPuvcSRZ9lEgxfHdM");
+  Geocode.setLanguage("en");
+
+  Geocode.enableDebug();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = ("Latitude is :", position.coords.latitude);
+      let lng = ("Longitude is :", position.coords.longitude);
+      console.log(lat, lng);
+      Geocode.fromLatLng(lat, lng).then(
+        (response) => {
+          const address = response.results[0].address_components[3].long_name;
+          setCity(address);
+          console.log(address);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    });
+  }, []);
+
+  const getMyLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = ("Latitude is :", position.coords.latitude);
+      let lng = ("Longitude is :", position.coords.longitude);
+      console.log(lat, lng);
+      Geocode.fromLatLng(lat, lng).then(
+        (response) => {
+          const address = response.results[0].address_components[3].long_name;
+          setCity(address);
+          console.log(address);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    });
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API}&units=metric`
+      )
+      .then((response) => {
+        handleResponse(response);
+      })
+      .catch((error) => {
+        alert("Please check the city name!", error);
+      });
+  }, [city]);
+
+  console.log("city", city);
 
   function handleSubmit(event) {
-    event.preventDefault();
-    search();
+    setCity(searcher);
   }
 
   function handleInput(event) {
+    event.preventDefault();
+    setSearcher(event.target.value);
+  }
+
+  function handleInputDefault(event) {
     setCity(event.target.value);
   }
 
   return (
     <Container className="Container" maxWidth="sm">
-      <div className="Weather">
-        <section className="Header">Hoe is het weer vandaag?</section>
-        <div className="search-section">
-          <form onSubmit={handleSubmit}>
+      {city ? (
+        <div className="Weather">
+          {weatherData.ready ? <Searchresult data={weatherData} /> : null}
+          <section className="Header">Hoe is het weer vandaag?</section>
+          <div className="search-section">
             <input
               type="search"
               placeholder="Voer een stadsnaam in"
@@ -48,50 +106,58 @@ export default function Weather(props) {
               autoFocus="on"
               onChange={handleInput}
             />
+            <button onClick={handleSubmit}>zoeken</button>
 
-            <input type="submit" value="zoeken" className="button" />
-            {weatherData.ready ? <Searchresult data={weatherData} /> : null}
             <div className="fixedButtons">
               <input
                 type="submit"
                 value="Amsterdam"
                 className="CityButton"
-                onClick={handleInput}
+                onClick={handleInputDefault}
               />
               <input
                 type="submit"
                 value="London"
                 className="CityButton"
-                onClick={handleInput}
+                onClick={handleInputDefault}
               />
               <input
                 type="submit"
                 value="New York"
                 className="CityButton"
-                onClick={handleInput}
+                onClick={handleInputDefault}
               />
               <input
                 type="submit"
                 value="Tokyo"
                 className="CityButton"
-                onClick={handleInput}
+                onClick={handleInputDefault}
               />
               <input
                 type="submit"
                 value="Paris"
                 className="CityButton"
-                onClick={handleInput}
+                onClick={handleInputDefault}
               />
               <input
                 type="submit"
                 value="Milan"
                 className="CityButton"
-                onClick={handleInput}
+                onClick={handleInputDefault}
+              />
+
+              <input
+                type="submit"
+                value="MyLocation"
+                className="CityButton"
+                onClick={getMyLocation}
               />
             </div>
-          </form>
+          </div>
         </div>
-      </div>
+      ) : (
+        <h1>Loading....</h1>
+      )}
     </Container>
   );
 }
